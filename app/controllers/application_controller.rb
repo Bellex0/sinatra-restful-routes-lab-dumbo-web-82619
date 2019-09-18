21dd1a174f740ba7c641f5499ffcc7d210f8ff98
Belle P <bellepx3@gmail.com>
2019-09-18 21:36:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,13 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/recipes/new' do
+  erb :new
+end
+ 
+post '/recipes' do
+  @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+  redirect to "/recipes/#{@recipe.id}"
+end
 
 end
